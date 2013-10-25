840c7876c6fcd3dcd3d745d8aee175d0ec01b5d2
pchiang <rukqoa@gmail.com>
2013-10-25 12:04:28 -0700

    added autocomplete & advanced options

--- js/index.js
+++ js/index.js
@@ -1,10 +1,28 @@
 $(function() {
+
+    $(".form-control").focus();
+
     $(".random").tooltip({
-        "placement" : "right",
+        "placement" : "left",
         "title" : "Click me for a good time"
     });
-    
+
+    $(".settings").hide();
     $(".advanced").click(function() {
-        $(".settings").toggle();
+        $(".settings").toggle(200);
+    });
+
+    $(".list-group-item").click(function() {
+        var checkbox = $(this).find("input[type='checkbox']");
+        checkbox.prop("checked", !checkbox.prop("checked"));
+    });
+
+    $(".list-group-item input[type=checkbox]").click(function(e) {
+        e.stopPropagation();
+    });
+
+    var availableTags = ["Chipotle Mexican Grill", "Taco Del Mar", "Korean Tofu House", "Green House Korean Restaurant", "UDon Fresh Japanese Noodle Station", "Samurai Noodle", "Teriyaki 1st", "Ichiro Teriyaki", "University Teriyaki", "Yummy Bites", "Orange King", "Hungry Husky", "A Burger Place", "Burger Hut", "Subway", "Jimmy John's", "Hungry Husky", "Pho Than Brothers", "Thanh Vi", "Shawarma King", "Aladdin Falafel Corner", "Thai Tom", "Little Thai Restaurant"];
+    $(".form-control").autocomplete({
+        lookup : availableTags
     });
 });
