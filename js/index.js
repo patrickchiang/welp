49e5c5d0d96ace9c7a536c367e2dbdacd58bb44f
patrickchiang <pchiang@uw.edu>
2013-12-06 03:08:35 -0800

    Get on it

--- js/index.js
+++ js/index.js
@@ -1,14 +1,26 @@
 var results, pageInfo;
 
 $(function() {
+	$(".explore").click(showModal);
 });
 
+function showModal() {
+	$(".modal-body").empty();
+	var categories = ["American", "Asian", "Fusion", "Barbeque", "Buffets", "Burgers", "Cafes", "Chinese", "Creperies", "Fast", "Food", "Food", "Stands", "Greek", "Hawaiian", "Indian", "Italian", "Japanese", "Korean", "Mediterranean", "Mexican", "Pizzar", "Salad", "Sandwiches", "Soup", "Teriyaki", "Thai", "Vietnamese"];
+	for (var i = 0; i < categories.length; i++) {
+		var temp = $(".template-category").clone().removeClass("template-category");
+		temp.find("button").html(categories[i]);
+		$(".modal-body").append(temp);
+	}
+	$('#myModal').modal();
+}
+
 function showPage() {
-    var name = $(this).attr("href").split("#")[1];
-    //$.get("page.php?name=" + name, displayPage);
-    $("link[href=\"css/none.css\"]").attr("href", "css/showpage.css");
+	var name = $(this).attr("href").split("#")[1];
+	//$.get("page.php?name=" + name, displayPage);
+	$("link[href=\"css/none.css\"]").attr("href", "css/showpage.css");
 }
 
 function displayPage(data) {
-    //pageInfo = JSON.parse(data);
+	//pageInfo = JSON.parse(data);
 }
